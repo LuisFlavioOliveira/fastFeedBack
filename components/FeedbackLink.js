20f4e1bc10674b784c22fc94547bbd93e92569d3
Luis Flávio Oliveira <luisflavio5@hotmail.com>
2021-01-02 20:12:50 +0100

    Added api function to fetch feedback data from firestore

---
+++ components/FeedbackLink.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import { Flex, Link } from '@chakra-ui/react';
+
+export default function FeedbackLink({ siteId }) {
+  return (
+    <Flex justifyContent="space-between" mb={8} w="full" my={1}>
+      <Link fontWeight="bold" fontSize="sm" href={`/p/${siteId}`}>
+        Leave a comment
+      </Link>
+      <Link fontSize="xs" color="blackAlpha.500" href="/">
+        Powered by Fast Feedback
+      </Link>
+    </Flex>
+  );
+}
